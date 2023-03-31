474b20201f8efb3c9f66f40f829099269d4e05f2
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2023-03-30 23:37:43 -0400

    s02-39 Creating a new, custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 // void main() {
 //   runApp(MyApp());
 // }
@@ -35,7 +37,7 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(
+            Question(
               questions[_questionIndex],
             ),
             ElevatedButton(
