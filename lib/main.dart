9a0c8d5dc56727094e7a504125c8d6549234a392
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2023-03-30 22:17:47 -0400

    s02-38 Using private properties

--- lib/main.dart
+++ lib/main.dart
@@ -5,22 +5,21 @@ import 'package:flutter/material.dart';
 // }
 void main() => runApp(MyApp());
 
-// ignore: must_be_immutable
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      questionIndex += 1;
+      _questionIndex += 1;
     });
-    print(questionIndex);
+    print(_questionIndex);
   }
 
   @override
@@ -37,11 +36,11 @@ class MyAppState extends State<MyApp> {
         body: Column(
           children: [
             Text(
-              questions[questionIndex],
+              questions[_questionIndex],
             ),
             ElevatedButton(
               child: Text('Answer 1'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
             ElevatedButton(
               child: Text('Answer 1'),
