0d12c538c9ba0ba35d104db4c471be76eaadefeb
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2023-04-01 16:38:33 -0400

    s02-46 Mapping lists to widgets

--- lib/answer.dart
+++ lib/answer.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final Function selectHandler;
+  final String answerText;
 
-  Answer(this.selectHandler);
+  Answer(this.selectHandler, this.answerText);
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +16,7 @@ class Answer extends StatelessWidget {
         //   backgroundColor: MaterialStateProperty.all(Colors.blue),
         //   foregroundColor: MaterialStateProperty.all(Colors.white),
         // ),
-        child: Text('Answer 1'),
+        child: Text(answerText),
         onPressed: selectHandler,
       ),
     );
