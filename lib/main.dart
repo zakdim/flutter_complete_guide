bcb22affc6b05f707460ac6ef733cc92d351de05
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2023-04-02 16:25:34 -0400

    s02-54 Getters & else-if

--- lib/main.dart
+++ lib/main.dart
@@ -75,7 +75,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(),
+            : Result(_totalScore),
       ),
     );
   }
